f09408ef24a8c8d6fe8d6f6065fdb82a4a76d8e4
Max Bradley <mbrad16@gmail.com>
2018-02-22 20:47:49 -0500

    first

---
+++ app/src/test/java/com/example/max/busapp/ExampleUnitTest.java
@@ -0,0 +1,24 @@
+package com.example.max.busapp;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+}
