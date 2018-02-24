224990e0d9385f8dfc3afe8f8d120aec497f442e
Max Bradley <mbrad16@gmail.com>
2018-02-24 00:05:00 -0500

    working implementation and cleaned up

--- app/src/main/java/com/example/max/busapp/MapsActivity.java
+++ app/src/main/java/com/example/max/busapp/MapsActivity.java
@@ -1,24 +1,58 @@
 package com.example.max.busapp;
 
+import android.*;
+import android.Manifest;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnCameraMoveListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private GroundOverlay car;
+
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private LocationCallback mLocationCallback;
+    private LocationRequest mLocationRequest;
+    private boolean mRequestingLocationUpdates;
+
+    final private int LOCATION_PERMISSION_REQUEST = 1;
+    final private int LOCATION_REQUEST_INTERVAL = 1000; //milliseconds for location request
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +63,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //set up location callback for frequent location updates
+        mLocationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for(Location location : locationResult.getLocations()) {
+                    //do updates with location
+                    if(car != null && car.isVisible() && location  != null)
+                    {
+                        //update car position on screen to match location
+                        car.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+                        //check bearing and set image based on it
+                        if(location.hasBearing()) {
+                            car.setBearing(location.getBearing());
+                        }
+                    }
+                }
+            }
+        };
 
-        FirebaseDatabase database = FirebaseHelper.getInstance().db;
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
-
 
     /**
      * Manipulates the map once available.
@@ -46,31 +97,125 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // Add a marker in Sydney and move the camera
-        LatLng baltimore = new LatLng(39.281516, -76.597448);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(baltimore,17));
-        BitmapDescriptor image = BitmapDescriptorFactory.fromResource(R.mipmap.redcartopviewhi);
-        car  = mMap.addGroundOverlay(new GroundOverlayOptions().image(image).position(baltimore,50f, 30f));
-        mMap.setOnCameraMoveListener(this);
+        MapIsReady();
+
+    }
+
+    /*Helper method so that the map can be reset once permissions have been granted */
+    private void MapIsReady() {
+        //request permission
+        if (checkPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            //location permission good
+            createLocationRequest();
+        }
+    }
+
+    /* callback method for when permissions request returns */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mRequestingLocationUpdates = true;
+                    MapIsReady();
+                } else {
+                    // permission denied
+                    mRequestingLocationUpdates = false;
+                }
+                return;
+            }
+        }
+    }
+
+    /* initialize location reuqest objects */
+    protected void createLocationRequest() {
+        //allows accuracy within a few feet (caused by combo of fine location, high accuracy, and 5 sec interval
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(LOCATION_REQUEST_INTERVAL);
+        mLocationRequest.setFastestInterval(LOCATION_REQUEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                String a = "";
+                //settings satisfied
+                mRequestingLocationUpdates = true;
+                startLocationUpdates();
+            }
+        });
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(e instanceof ResolvableApiException){
+                    //settings not satisfied, show user dialog
+                    try{
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MapsActivity.this,
+                                100 );
+                    }catch(IntentSender.SendIntentException sendEx){
+                        //ignore it
+                    }
+                }
+            }
+        });
     }
 
     @Override
-    public void onCameraMove()
-    {
-        if(car != null && car.isVisible() && mMap != null)
-        {
-            car.setPosition(mMap.getCameraPosition().target);
+    protected void onResume() {
+        super.onResume();
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
         }
     }
 
-    /*Called when Go Button clicked*/
-    public void startRoute(View view)
-    {
-        if(!FirebaseHelper.latLongList.isEmpty())
-        {
-            LatLong loc1 = FirebaseHelper.latLongList.get(0);
-            mMap.moveCamera(CameraUpdateFactory.scrollBy(400,0));
+    private void startLocationUpdates() {
+        if(checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                //setting initial map view and car image
+                                LatLng currLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currLocation, 17));
+                                BitmapDescriptor image = BitmapDescriptorFactory.fromResource(R.mipmap.redcartopviewhi);
+                                car = mMap.addGroundOverlay(new GroundOverlayOptions().image(image).position(currLocation, 30f, 50f));
+                            }
+                        }
+                    });
+            mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest,
+                    mLocationCallback,
+                    null /* Looper */);
         }
     }
 
+    /* helper to check permissions */
+    private boolean checkPermission(String permission)
+    {
+        if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            //location permission not granted
+            ActivityCompat.requestPermissions(this, new String[]{permission}, LOCATION_PERMISSION_REQUEST);
+            return false; //don't want to try and make any location requests until allowed
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
+    }
 }
